40278b06be778df08d348c5089274c47a283abbc
李心汝 <lixinru@lixinrudeMacBook-Air.local>
2021-09-21 21:20:56 +0800

    change to wile i>0

--- password1.py
+++ password1.py
@@ -1,7 +1,7 @@
 password = 'a123456' #先將答案存起來了
 i = 3 #剩餘機會
 
-while True:
+while i > 0:                    # 若不寫 true 可以怎麼寫？從剩餘機會思考
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('成功登入')
@@ -9,5 +9,3 @@ while True:
 	else:
 		i = i - 1
 		print('密碼錯誤! 還有', i,'次機會') # 要拆開，字串、整數、字串
-		if i == 0:
-			break
